7c8c47ab27a3271a5788a6a1ce6d156f4f30b24a
Ankur Raj <ankur@salesphin.com>
2024-04-28 05:54:27 +0530

    initial changes

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
 
-app.get('/home', (req,res)=>{
-    res.json({message: "OKK"});
+app.get('/home', (req, res) => {
+    res.json({ message: "OKK" });
 });
 
-app.listen(5000, ()=>{
+app.listen(process.env.PORT, () => {
     console.log("Started the server");
 })
